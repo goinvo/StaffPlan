1d1eb9fcae282ba9a58f859ba93e02a787d6e236
Rob Sterner <rob.sterner@gmail.com>
2011-12-19 09:15:48 -0500

    add and configure rspec, cucumber, barista, compass, etc.

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module StaffPlan
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -50,5 +50,14 @@ module StaffPlan
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec, :fixture => true, :views => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+    
+    config.sass.syntax = :sass
+    config.sass.style  = :compressed
   end
 end
