d69794ba177a4c00a4c2aa6aab8bbdaf556dbf8f
Rob Sterner <rob.sterner@gmail.com>
2011-12-20 06:11:42 -0500

    remove some config options for sass, they seem to be invalid and failing silently in development anyway

--- config/application.rb
+++ config/application.rb
@@ -56,8 +56,5 @@ module StaffPlan
       g.test_framework :rspec, :fixture => true, :views => false
       g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
-    
-    config.sass.syntax = :sass
-    config.sass.style  = :compressed
   end
 end
