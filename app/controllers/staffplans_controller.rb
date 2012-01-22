9c3850893e85966796d5990369fd572e55114fab
Rob Sterner <rob.sterner@gmail.com>
2012-01-22 14:40:16 -0500

    remove ruby debug from spec_helper, fix specs

--- app/controllers/staffplans_controller.rb
+++ app/controllers/staffplans_controller.rb
@@ -2,7 +2,7 @@ class StaffplansController < ApplicationController
   
   def show
     @target_user = User.where(id: params[:id]).first
-    redirect_to root_url, error: I18n.t('controllers.staffplans.couldnt_find_user') unless @target_user.present?
+    redirect_to root_url, error: I18n.t('controllers.staffplans.couldnt_find_user') and return unless @target_user.present?
     
     @target_user_json = @target_user.staff_plan_json
     @clients = Client.joins(:projects).all
