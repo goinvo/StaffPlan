9e340cf0e1df4a24790fb2e64b33b928522ef489
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-02-21 12:06:15 -0800

    Fixed scoping for users
    UsersController#index now returns users for the current company of the
    current user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.where(current_company: current_user.current_company).all
-
+    @users = current_user.current_company.users
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
