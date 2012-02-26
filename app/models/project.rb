5efa23ba9b04121d3a4eebf73ae89ad2e3118923
Rob Sterner <rob.sterner@gmail.com>
2012-02-26 16:44:12 -0500

    Formatting + a new scope on the work_weeks association.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,13 @@
 class Project < ActiveRecord::Base
   
   belongs_to  :client
-  has_many :work_weeks, :dependent => :destroy do
+  has_many :work_weeks, dependent: :destroy do
     def for_user(user)
       self.select { |ww| ww.user_id == user.id }
+    end
+    
+    def for_user_and_cweek_and_year(user, cweek, year)
+      self.for_user(user).detect { |ww| ww.cweek == cweek && ww.year == year }
     end
   end
   
