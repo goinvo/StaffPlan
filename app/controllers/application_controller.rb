9d3c732a1bdafd3cc3761234c736fb9b2786a2fe
Rob Sterner <rob.sterner@gmail.com>
2012-02-27 17:03:38 -0500

    Toggle between mobile/full site view.  Staffplan formatting still looks wonky on the iPad simulator.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery
   has_mobile_fu
-  before_filter :require_current_user
+  before_filter :require_current_user, :set_mobile_view
   
   private
   
@@ -13,5 +13,12 @@ class ApplicationController < ActionController::Base
   
   def require_current_user
     redirect_to new_session_url unless current_user.present?
+  end
+  
+  def set_mobile_view
+    if params[:mobile_view].present?
+      session[:mobile_view] = params[:mobile_view] == "true"
+      set_mobile_format
+    end
   end
 end
