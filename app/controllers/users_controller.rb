7a6d4fc0e72b90807fa2e5a449339a7bbdadaecd
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-02-29 21:28:28 -0800

    Added Jbuilder to project
    - Created JSON template for UsersController#show
    - Created partial for projects, used in aforementioned UsersController#show JSON
      template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
   end
 
   # GET /users/new
