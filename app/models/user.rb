81040633300f209d56697df5195b723e557c510d
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-02-29 22:49:25 -0800

    Refactoring of User#staff_plan_json using Jbuilder
    Still think this calls for a decorator or a presenter which would hold
    the different JSON representations used across the app for a given
    object.

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,30 +25,40 @@ class User < ActiveRecord::Base
 
   # TODO: custom finder SQL
   def staff_plan_json
-    json = {
-      id: self.id,
-      name: self.name,
-      email: self.email,
-      gravatar: self.gravatar,
-      projects: []
-    }
-
-    self.projects.each do |project|
-      json[:projects] << {
-        id: project.id,
-        name: project.name,
-        client_id: project.client_id,
-        work_weeks: project.work_weeks.for_user(self).map do |ww|
-        { id: ww.id,
-          project_id: ww.project_id,
-          actual_hours: ww.actual_hours,
-          estimated_hours: ww.estimated_hours,
-          cweek: ww.cweek,
-          year: ww.year }
+    Jbuilder.encode do |json|
+      json.(self, :id, :name, :email, :gravatar)
+      json.projects self.projects do |json, project|
+        json.(project, :id, :name, :client_id)
+        json.work_weeks project.work_weeks.for_user(self) do |json, work_week|
+          json.(work_week, :id, :project_id, :actual_hours, :estimated_hours, :cweek, :year)
         end
-      }
+      end
     end
-
-    json.to_json
   end
+    # json = {
+    #   id: self.id,
+    #   name: self.name,
+    #   email: self.email,
+    #   gravatar: self.gravatar,
+    #   projects: []
+    # }
+
+    # self.projects.each do |project|
+    #   json[:projects] << {
+    #     id: project.id,
+    #     name: project.name,
+    #     client_id: project.client_id,
+    #     work_weeks: project.work_weeks.for_user(self).map do |ww|
+    #     { id: ww.id,
+    #       project_id: ww.project_id,
+    #       actual_hours: ww.actual_hours,
+    #       estimated_hours: ww.estimated_hours,
+    #       cweek: ww.cweek,
+    #       year: ww.year }
+    #     end
+    #   }
+    # end
+
+    # json.to_json
+    # end
 end
