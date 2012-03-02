4e65f4d9d40a5e64374518b5a1e0faa7bd0b9b73
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-03-01 18:02:50 -0800

    Fix for empty staffplan page
    Since the format of the @clients array we pass to the view changed (due
    to Jbuilder returning an array of JSON strings) we have to convert those
    strings into objects for the client to consume.
    
    SPECS:
    ------
    
    All specs are passing with 11 view specs pending.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,18 +1,15 @@
 class Client < ActiveRecord::Base
   has_many :projects
   belongs_to :company
-  
+
   validates_presence_of :name
-  
+
   default_scope(order: "name ASC")
-  
+
   def staff_plan_json
     Jbuilder.encode do |json|
       json.(self, :id, :name, :active)
-      json.projects self.projects do |json, project|
-        attrs = (Project.columns_hash.keys - %w(updated_at created_at id)).map(&:to_sym)
-        json.(project, *attrs)
-      end
+      json.projects self.projects
     end
   end
 end
