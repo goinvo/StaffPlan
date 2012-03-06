53de85aebf34203a0dfeb6a336c9191b9ec920f4
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-03-05 19:44:30 -0800

    First working draft of company form
    Still missing:
    
    - form_for doesn't really use the association and submits a form with two
      objects that I have to knit back together in the create action
    - Styling isn't quite right
    - Need to add a field for password confirmation
    - Need to refactor the code for the create action

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,18 @@
 class CompaniesController < ApplicationController
   def new
-
+    @company = Company.new 
+    @user = @company.users.build
   end
 
   def create
-
+    @company = Company.new(params[:company])
+    @user = User.where(email: params[:user][:email]).first
+    @user ||= User.create(params[:user])
+    @company.users << @user
+    if @company.save!
+      redirect_to root_url, notice: "Company was successfully created"
+    else
+      redirect_to :back, notice: "oops, something went wrong"
+    end
   end
 end
