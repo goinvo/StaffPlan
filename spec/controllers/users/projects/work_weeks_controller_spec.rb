f795ee4237e6756b13bf79e8e8b473aedef359e3
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-03-09 09:51:44 -0800

    Added test for WorkWeekController#create

--- spec/controllers/users/projects/work_weeks_controller_spec.rb
+++ spec/controllers/users/projects/work_weeks_controller_spec.rb
@@ -13,12 +13,23 @@ describe Users::Projects::WorkWeeksController do
 
   describe "all actions" do
     it "should redirect if the target user can't be found" do
-
     end
   end
 
   describe '#create' do
-
+    it "should return the JSON OK response for a successful work_week creation" do
+      post(:create, {
+        user_id: @user.id, 
+        project_id: @project.id, 
+        cweek: Date.today.cweek, 
+        year: Date.today.year, 
+        format: "js"
+      })
+      assigns[:project].should be_a(Project)
+      @body = JSON.parse(response.body)
+      @body.should be_a(Hash)
+      @body['status'].should eq("ok")
+    end
   end
 
   describe '#update' do
@@ -26,11 +37,17 @@ describe Users::Projects::WorkWeeksController do
     before(:each) do
       @work_week = Factory(:work_week, user: @user, project: @project)
     end
-    
+
     context "format: 'js'" do
       it "should render the fail JSON response if update_attributes fails" do
         WorkWeek.any_instance.stubs(:update_attributes).returns(false) 
-        put :update, user_id: @user.id, project_id: @project.id, id: @work_week.id, cweek: 19, format: 'js'
+        put :update, {
+          user_id: @user.id, 
+          project_id: @project.id, 
+          id: @work_week.id, 
+          cweek: 19, 
+          format: 'js'
+        }
         @body = JSON.parse(response.body)
         @body.class.should eq(Hash)
         @body['status'].should eq("fail")
@@ -48,33 +65,54 @@ describe Users::Projects::WorkWeeksController do
           bullshit_hours: 24,
           velocity: 34
         }
-        # Not sure what to even test here... Shouldn't the whitelist_attributes method itself be tested instead?
-        # No, it's a private method.  Private methods are tested indirectly by properly testing the public methods that call them.
+        # Not sure what to even test here... Shouldn't the whitelist_attributes 
+        # method itself be tested instead?
+        # No, it's a private method.  Private methods are tested indirectly 
+        # by properly testing the public methods that call them.
         # For example: params = {some: "gibberish", like: "this", year: "1999", actual_hours: 9}
-        # then the WorkWeek instance should only receive the last two K/V pairs in the params hash (year, actual_hours)
+        # then the WorkWeek instance should only receive the 
+        # last two K/V pairs in the params hash (year, actual_hours)
       end
 
       it "should render the OK JSON if update_attributes passes" do
         WorkWeek.any_instance.expects(:update_attributes).with(anything).returns(true)
-        put :update, user_id: @user.id, project_id: @project.id, id: @work_week.id, cweek: 19, format: 'js'
+        put :update, {
+          user_id: @user.id, 
+          project_id: @project.id, 
+          id: @work_week.id, 
+          cweek: 19, 
+          format: 'js'
+        }
         @body = JSON.parse(response.body)
         @body.class.should eq(Hash)
         @body['status'].should eq("ok")
       end
     end
-    
+
     context "format: 'mobile'" do
       it "should render the project partial if the update fails" do
         # Error handling/messaging is TBD
         WorkWeek.any_instance.stubs(:update_attributes).returns(false) 
-        put :update, user_id: @user.id, project_id: @project.id, id: @work_week.id, cweek: 19, format: 'mobile'
+        put :update, {
+          user_id: @user.id, 
+          project_id: @project.id, 
+          id: @work_week.id, 
+          cweek: 19, 
+          format: 'mobile'
+        }
         response.should be_success
         response.should render_template("staffplans/_project")
       end
-      
+
       it "should render the project partial if the update succeeds" do
         WorkWeek.any_instance.stubs(:update_attributes).returns(true)
-        put :update, user_id: @user.id, project_id: @project.id, id: @work_week.id, cweek: 19, format: 'mobile'
+        put :update, {
+          user_id: @user.id, 
+          project_id: @project.id, 
+          id: @work_week.id, 
+          cweek: 19, 
+          format: 'mobile'
+        }
         response.should be_success
         response.should render_template("staffplans/_project")
       end
