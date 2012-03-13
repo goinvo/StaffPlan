7656ba723f92aa7e17b3ec9793d802c09434b362
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-03-13 09:19:02 -0700

    Refactored unit tests for User#plan_for
    Tested +/- cases and DRY'd it up

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ describe User do
     it "should not be valid? for new instances" do
       User.new.valid?.should be_false
     end
-    
+
     it "should be valid? with valid attributes" do
       User.new(
         name: Faker::Name.name,
@@ -15,42 +15,54 @@ describe User do
         password_confirmation: 'password'
       ).valid?.should be_true
     end
-    
+
     it "should validate the email format" do
       User.create(email: "thisisnotanemailaddress").errors[:email].should_not be_empty
     end
 
-    it "should return 0 when I try to get the number of estimated hours for a new user" do
+  end
+
+  describe 'User#plan_for' do
+    before(:each) do
       @user = User.new(email: Faker::Internet.email, name: Faker::Name.name, password: "23kj23k2j")
       @company = Factory(:company)
       @company.users << @user
       @user.current_company_id = @company.id
       @user.save
 
-      @user.plan_for(@company.id).should eq(0)
-      
       @client = Client.new(name: Faker::Company.name, description: Faker::Lorem.sentences(2), active: true)
       @client.company_id = @company.id
       @client.save
-      
+
       @project = Project.new(name: Faker::Company.name, name: Faker::Company.bs)
       @project.company_id = @company.id
       @project.client_id = @client.id
       @project.save
 
-      @work_week = WorkWeek.new(cweek: 2.weeks.from_now(Date.today).cweek, year: Date.today.year, estimated_hours: 20, actual_hours: 12)
+    end 
+
+    it "should return 0 when I try to get the number of estimated hours for a new user" do
+      @user.plan_for(@company.id).should eq(0)
+    end
+
+    it "should return 0 when the user hasn't put any estimates for the future" do
+      @work_week = WorkWeek.new(cweek: 2.months.ago(Date.today).cweek, year: Date.today.year, estimated_hours: 20, actual_hours: 12)
       @work_week.project_id = @project.id
       @work_week.user_id = @user.id
       @work_week.save
-      @user.plan_for(@company.id).should eq(20)
-
-      @work_week.update_attributes(estimated_hours: 200)
-      @user.plan_for(@company.id).should eq(200)
-
-      @work_week.update_attributes(year: 2.years.ago(Date.today).year)
       @user.plan_for(@company.id).should eq(0)
+    end
 
-      
+    it "should return the sum of workload estimates for the future" do
+      @w1 = WorkWeek.new(cweek: 2.months.from_now(Date.today).cweek, year: Date.today.year, estimated_hours: 20, actual_hours: 12)
+      @w1.project_id = @project.id
+
+      @w2 = WorkWeek.new(cweek: 1, year: 3.years.from_now(Date.today).year, estimated_hours: 80, actual_hours: 12)
+      @w2.project_id = @project.id
+
+      @user.work_weeks << [@w1, @w2]
+      @user.save
+      @user.plan_for(@company.id).should eq(100)
     end
   end
 end
