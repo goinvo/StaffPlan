291385ea9b03fa939cba6b8dd5871d5c3cabf299
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-03-12 18:06:36 -0700

    Story #136750 Change order on All Staff page
    - Added a scope to retrieve work_weeks for a user and a company starting
      from the beginning of the current week
    - A user method then uses that scope to compute total hours for those
      work_weeks
    - The users are then sorted in the controller using that method
    - Modified spec, since the order of the elements in @users in the index
      action has changed.

--- app/models/work_week.rb
+++ app/models/work_week.rb
@@ -6,4 +6,8 @@ class WorkWeek < ActiveRecord::Base
   validates_presence_of :user, :project
   validates_numericality_of :estimated_hours, :actual_hours, greater_than_or_equal_to: 0, allow_nil: true
 
+  scope :for_user_and_company, lambda { |user_id, company_id|
+    joins(:project).where("projects.company_id = ? AND work_weeks.user_id = ? AND cweek > ?", company_id, user_id, Date.today.at_beginning_of_week.cweek)
+  }
+
 end
