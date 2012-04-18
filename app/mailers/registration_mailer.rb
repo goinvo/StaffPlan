8f1a7d6d85c47adfe0cdbb9030223a2213c58107
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-04-18 09:49:19 -0700

    Adding sender informaiton in mailer

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,18 +1,18 @@
 class RegistrationMailer < ActionMailer::Base
   def registration_confirmation user
     @user = user
-    mail to: @user.email, subject: "Welcome to StaffPlan"
+    mail to: @user.email, from: "notifier@staffplan.com", subject: "Welcome to StaffPlan"
   end
 
   def invitation(user, inviting_user)
     @user = user
     @admin = inviting_user 
-    mail to: @user.email, subject: "You're invited to join our project planning and collaboration system"
+    mail to: @user.email, from: "notifier@staffplan.com", subject: "You're invited to join our project planning and collaboration system"
   end
 
   def password_reset user
     @user = user
-    mail to: @user.email, subject: "How to reset your StaffPlan password"
+    mail to: @user.email, from: "notifier@staffplan.com", subject: "How to reset your StaffPlan password"
   end
 
 end
