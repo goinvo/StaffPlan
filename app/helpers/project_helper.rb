56edc8810ea27b8e4e76304919b458e7a03682d8
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-05-02 21:45:26 -0700

    Adjusting the relative height of the bars

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -2,11 +2,11 @@ module ProjectHelper
   def chart_for_project(project, date_range, totals_per_week, max_size)
     capture_haml do 
       date_range.each do |date|
-        ratio = max_size == 0 ? 1 : (100.to_f / max_size.to_f)
+        ratio = (max_size == 0) ? 1 : (100.to_f / max_size.to_f)
         css_class = ((date <= Date.today) ?  'passed' : '')
         actual_or_estimated_hours = date.past? ? :actual_hours : :estimated_hours
         total = totals_per_week.try(:[], date.year).try(:[], date.cweek) || 0 
-        height = (total.to_f * ratio).floor 
+        height = (total.to_f * ratio.to_f).floor 
         haml_tag :li, {:class => css_class, :style => "height: #{height}px"} do
           haml_tag :span do 
             haml_concat total.to_s
