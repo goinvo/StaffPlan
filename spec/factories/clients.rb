7b38606a4c8f71b37ccd88b58f813d3fd3651859
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-05-02 22:17:21 -0700

    Fixing spec error

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -1,8 +1,14 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
-
+f = Fiber.new do
+  x = 1
+  loop do
+    Fiber.yield x
+    x += 1
+  end
+end
 FactoryGirl.define do
   factory :client do
-    name          { Faker::Company.name + Faker::Company.name } 
-    description   { Faker::Company.catch_phrase + Faker::Company.name } 
+    name          { Faker::Company.name + f.resume.to_s } 
+    description   { Faker::Company.catch_phrase + f.resume.to_s } 
   end
 end
