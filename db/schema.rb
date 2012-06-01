91e1d9f7c8d38538119d18dd3f9ea6c16eaab101
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-06-01 16:28:57 -0700

    Removed proposed field from projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120601230119) do
+ActiveRecord::Schema.define(:version => 20120601232645) do
 
   create_table "assignments", :force => true do |t|
     t.integer "user_id"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(:version => 20120601230119) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "company_id"
-    t.boolean  "proposed",   :default => false, :null => false
   end
 
   create_table "projects_users", :id => false, :force => true do |t|
