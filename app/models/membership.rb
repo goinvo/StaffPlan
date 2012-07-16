aaabac3b0c863ef608b031b7443172928a7ae7c6
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-07-16 15:23:51 -0700

    Small fix

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -6,7 +6,7 @@ class Membership < ActiveRecord::Base
     if record.disabled?
       u = record.user
       # We're disabling that user for the very last company he's attached to
-      if u.selectable_companies.count == 1 and u.current_company == company
+      if u.selectable_companies.count == 1 and u.current_company == record.company
         u.current_company = nil  
       else
         u.current_company = (u.selectable_companies - [u.current_company]).first
