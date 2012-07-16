21f88ca27b467108be9865299717ab1e9343feab
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-07-16 16:18:23 -0700

    Making sure that we have membership ready
    Dealing with users and relations between users and companies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
         format.html { redirect_to @user, notice: "Invitation successfully sent to #{@user.full_name}" }
         format.json { render json: @user, status: :created, location: @user }
       else
-        format.html { render action: "new" }
+        format.html { @membership = @user.memberships.build(company_id: @user.current_company_id); render action: "new" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -55,6 +55,9 @@ class UsersController < ApplicationController
   # PUT /users/1
   # PUT /users/1.json
   def update
+    if [:password, :password_confirmation].all? { |m| params[:user][m].blank? }
+      params[:user].delete_if {|k,v| k =~ /password/ }
+    end
     respond_to do |format|
       if new_current_company_id = params[:user].present? ? params[:user].delete(:current_company_id) : nil
         company = current_user.companies.find_by_id(new_current_company_id)
@@ -63,13 +66,12 @@ class UsersController < ApplicationController
         @user.save
         redirect_to :back and return
       end
-      
-      if @user.update_attributes(params[:user].except(:membership))
+      if @user.attributes = params[:user].except(:membership) and @user.save_unconfirmed_user
         @user.memberships.where(company_id: current_user.current_company_id).first.update_attributes(params[:user][:membership])
         format.html { redirect_to @user, notice: "User was successfully updated" }
         format.json { head :ok }
       else
-        format.html { render action: "edit" }
+        format.html { @membership = @user.memberships.where(company_id: @user.current_company_id).first; render action: "edit" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
