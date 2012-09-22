f7a13a978d9b8bdede1db576e2ecd3faee794f5f
Rob Sterner <rob.sterner@gmail.com>
2012-09-22 16:07:01 -0700

    We don't need any of this.

--- app/controllers/staffplans_controller.rb
+++ app/controllers/staffplans_controller.rb
@@ -1,15 +1,8 @@
 class StaffplansController < ApplicationController
   
   def show
-    @from = Date.parse(params[:from] || '').at_beginning_of_week rescue Date.today.at_beginning_of_week
-    @from = 1.week.ago(@from)
-    @target_user = UserDecorator.new(current_user.current_company.users.find_by_id(params[:id]))
-    redirect_to root_url, error: I18n.t('controllers.staffplans.couldnt_find_user') and return unless @target_user.model.present?
-    
     respond_to do |format|
-      format.html do
-      end
-    
+      format.html {}
       format.mobile do
         @date = (Date.parse(params[:date]) rescue Date.today).at_beginning_of_week
         @projects = @target_user.projects.group_by { |project| project.client.name }
@@ -23,29 +16,6 @@ class StaffplansController < ApplicationController
   end
 
   def index
-    @start = params[:from] # We need to save that for the view
-    @sort ||= params[:sort]
-    @from = Date.parse(params[:from] || '').at_beginning_of_week rescue Date.today.at_beginning_of_week
-    @from = 1.week.ago(@from)
-    @to = 3.months.from_now(@from)
-    
-    @date_range = []
-    start = @from.clone
-    
-    while start < @to
-      @date_range << start
-      start = start + 7.days
-    end
-    project_ids = current_user.current_company.projects.map(&:id)
-    
-    u = current_user.current_company.active_users.sort do |a, b|
-      if @sort.nil? || @sort == "workload"
-        a.plan_for(project_ids, @date_range.first) <=> b.plan_for(project_ids, @date_range.first)
-      else
-        a.last_name <=> b.last_name
-      end 
-    end
-    @users = UserDecorator.decorate(u)
   end
   
   def my_staffplan
