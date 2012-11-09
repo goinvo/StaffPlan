4377333beb7ab64baf84e337ba4c40f7c5c5f3ef
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-11-09 12:40:40 -0800

    Created method assignments_as_json

--- app/decorators/company_decorator.rb
+++ app/decorators/company_decorator.rb
@@ -2,18 +2,35 @@ class CompanyDecorator < Draper::Base
   decorates :company
 
   def clients_as_json
-    # Cheap and grouped by client id
     company_projects = model.projects.group_by(&:client_id)
     
     Jbuilder.encode do |json|
       json.array! model.clients do |json, client|
       json.(client, :id, :name, :description, :active)
-        # We've got it in the hash, fetch it
         json.projects company_projects[client.id]
       end
     end
   end
   
+  def assignments_as_json
+    
+    assignments = Assignment.includes(:work_weeks).where(
+      :user_id => self.users.map(&:id), 
+      :project_id => self.projects.map(&:id)
+    )
+
+    Jbuilder.encode do |json|
+      json.array! assignments do |json, assignment|
+        json.(assignment, :id, :user_id, :project_id, :proposed)
+        json.client_id assignment.project.client_id
+        json.work_weeks assignment.work_weeks do |json, work_week|
+          json.(work_week, :id, :actual_hours, :estimated_hours, :cweek, :year)
+          json.proposed assignment.proposed 
+        end
+      end
+    end
+  end
+
   def self_as_json
     Jbuilder.encode do |json|
       json.(self, :id, :name)
@@ -23,27 +40,20 @@ class CompanyDecorator < Draper::Base
   def projects_as_json
     Jbuilder.encode do |json|
       json.array!(model.projects) do |json, project|
-        json.(project, :id, :client_id, :name, :active, :proposed, :cost, :payment_frequency, :company_id)
+        json.(project, :id, :client_id, :name, :active, :cost, :payment_frequency, :company_id)
       end
     end
   end
   
   def users_json
     Jbuilder.encode do |json|
-      json.array!(model.users) do |json, user|
-        user = user.decorate
-        user_assignments = user.assignments.for_company(model)
-    
+      company_memberships = Membership.includes(:user).where(
+        :company_id => model.id
+      ) 
+      json.array!(company_memberships) do |json, membership|
+        user = membership.user.decorate
         json.(user, :id, :first_name, :last_name, :full_name, :email, :gravatar, :current_company_id)
-        json.membership model.memberships.where(:user_id => user.id).first
-        json.assignments user_assignments do |json, assignment|
-          json.(assignment, :id, :user_id, :project_id, :proposed)
-          json.client_id assignment.project.client_id
-          json.work_weeks assignment.work_weeks do |json, work_week|
-            json.(work_week, :id, :actual_hours, :estimated_hours, :cweek, :year)
-            json.proposed assignment.proposed 
-          end
-        end
+        json.membership membership 
       end
     end
   end
