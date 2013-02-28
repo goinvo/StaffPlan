368307c33d3452f99a937a241d5182f840c5b897
Bertrand Chardon <bertrand.chardon@gmail.com>
2013-02-27 16:01:52 -0800

    Bug [#45093187] Juhan has too many columns
    Cleans up the DB (removes duplicates and such) then adds an index on
    (assignment_id, beginning_of_week) so that no duplicate can be inserted
    in the future.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130204034907) do
+ActiveRecord::Schema.define(:version => 20130227192027) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "user_id"
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(:version => 20130204034907) do
     t.string   "name"
     t.text     "description"
     t.boolean  "active",      :default => true
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "company_id"
   end
 
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(:version => 20130204034907) do
     t.integer  "client_id"
     t.string   "name"
     t.boolean  "active",                                           :default => true
-    t.datetime "created_at",                                                            :null => false
-    t.datetime "updated_at",                                                            :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "company_id"
     t.boolean  "proposed",                                         :default => false,   :null => false
     t.decimal  "cost",              :precision => 12, :scale => 2, :default => 0.0,     :null => false
@@ -77,8 +77,8 @@ ActiveRecord::Schema.define(:version => 20130204034907) do
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "current_company_id"
     t.string   "registration_token"
     t.string   "first_name"
@@ -99,12 +99,14 @@ ActiveRecord::Schema.define(:version => 20130204034907) do
   create_table "work_weeks", :force => true do |t|
     t.integer  "estimated_hours"
     t.integer  "actual_hours"
-    t.integer  "cweek",             :limit => 2
-    t.integer  "year",              :limit => 2
-    t.datetime "created_at",                                                    :null => false
-    t.datetime "updated_at",                                                    :null => false
+    t.integer  "cweek"
+    t.integer  "year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "assignment_id"
-    t.decimal  "beginning_of_week",              :precision => 15, :scale => 0
+    t.decimal  "beginning_of_week", :precision => 15, :scale => 0
   end
+
+  add_index "work_weeks", ["assignment_id", "beginning_of_week"], :name => "index_work_weeks_on_assignment_id_and_beginning_of_week", :unique => true
 
 end
