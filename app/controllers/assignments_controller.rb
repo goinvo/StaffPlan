b853dfcf423a81c97e9c74eec37a8e50d5831001
Rob Sterner <rob.sterner@gmail.com>
2013-07-25 08:43:06 -0400

    clean up the controller a bit. Remove some global queries where they shouldn't exist. #125

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -3,35 +3,48 @@ class AssignmentsController < ApplicationController
   respond_to :json, :html
 
   def create
-    
-    if params[:assignment][:user_id].present?
-      @target_user = User.where(:id => params[:assignment][:user_id]).first 
-      @assignment = @target_user.assignments.build(params[:assignment])
+    if params[:user_id].present?
+      @target_user = current_user.current_company.users.find(params[:user_id])
+      
+      if @assignment = @target_user.assignments.where(project_id: params[:project_id]).first
+        # unarchive if they've previously archived and are re-adding it.
+        @assignment.assign_attributes(archived: false)
+      else
+        @assignment = @target_user.assignments.build(params[:assignment])
+      end
     else
-      @assignment = Project.where(:id => params[:assignment][:project_id]).first.assignments.build(params[:assignment])
+      # TBD assignment
+      project = current_user.current_company.projects.find(params[:project_id])
+      
+      if project.present?
+        @assignment = project.assignments.build(params[:assignment])
+      else
+        render(json: {status: "fail"}) and return
+      end
     end
     
-    if @assignment.save
-      respond_with @assignment and return
-    else
-      render :json => {:status => :unprocessable_entity }
-    end
+    @assignment.save
+    
+    respond_with(@assignment)
   end
 
   def update
     # The proposed and archived fields are the ONLY ones we can update
-    @assignment = Assignment.where(:id => params[:id]).first
-    if @assignment.update_attributes params[:assignment]
-      respond_with @assignment and return
-    else
-      render :json => {:status => :unprocessable_entity }
-    end
+    @assignment = current_user.current_company.assignments.find(params[:id])
+    @assignment.update_attributes(params[:assignment])
+    respond_with(@assignment)
   end
 
   def destroy
-    @assignment = Assignment.where(id: params[:id]).first
-    @assignment.destroy
-    render :json => { :status => :ok }  
+    @assignment = current_user.current_company.assignments.find_by_id(params[:id])
+    
+    if @assignment.blank?
+      @assignment = Assignment.find(params[:id])
+      # special case for TBD for now
+      @assignment = nil if @assignment.user_id.present? || @assignment.project.company != current_user.current_company
+    end
+    
+    @assignment.try(:destroy)
+    render(:json => { :status => :ok })
   end
-  
 end
