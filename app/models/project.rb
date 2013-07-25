9d3c40f543b598e9863999102d67d66c9612c244
Rob Sterner <rob.sterner@gmail.com>
2013-07-24 21:37:31 -0400

    Fixes #130, couldn't create a new project from /projects/new. More details in the issue.

--- app/models/project.rb
+++ app/models/project.rb
@@ -21,5 +21,11 @@ class Project < ActiveRecord::Base
   def proposed_for_user?(user)
     assignments.where(user_id: user.id).first.try(:proposed?) || false
   end
-
+  
+  # DB column can't be null but incoming params may pass it. If the key's present
+  # AR uses the value whatever it is, even if it's "". This doesn't trigger
+  # the default value of 0.0 to be set. Ignore blank values.
+  def cost=(new_cost)
+    assign_attributes(cost: new_cost) unless new_cost.blank?
+  end
 end
